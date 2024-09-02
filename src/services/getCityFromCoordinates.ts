c03edb1ff3a0eb62255a3f78b96349dd1c51cc70
lucashamaguchi <lucas@hamaguchi.com.br>
2024-09-02 04:00:28 -0300

    clean up commented code

--- src/services/getCityFromCoordinates.ts
+++ src/services/getCityFromCoordinates.ts
@@ -5,9 +5,6 @@ async function getCityFromCoordinates(coords: {
   lng: number | null;
 }) {
   const key = "9d53576de8ef6260ada1e512293dca3a";
-  // const response = await axios.get<any>(
-  //   `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${key}`
-  // );
   const response = await axios.get<any>(
     `https://api.openweathermap.org/geo/1.0/reverse?lat=${coords.lat}&lon=${coords.lng}&appid=${key}`
   );
--- src/services/getCoordsFromCity.ts
+++ src/services/getCoordsFromCity.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 async function getCoordsFromCity(address: string) {
   const key = "9d53576de8ef6260ada1e512293dca3a";
-  // const response = await axios.get<any>(
-  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`
-  // );
   const response = await axios.get<any>(
     `https://api.openweathermap.org/geo/1.0/direct?q=${address}&appid=${key}`
   );
